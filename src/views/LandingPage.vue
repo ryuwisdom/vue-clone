72b66074563fad67bb0bc18e4c54f957168a1852
류지혜 <ryujihye@lyujihyeui-MacBook-Pro.local>
2021-03-16 08:52:42 +0900

    practice box-model

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -16,146 +16,237 @@
       <p>ticker</p>
     </div>
     <div class="mainContents_container">
-      <p>main contents components place</p>
-      <div class="mainContents_inner_container">
-        <div class="mainComponent_box">
-          <Disclosures />
-          <CryptoAssets />
-          <MarketWatch />
-          <PRResearch />
+      <div class="top_items">
+        <div class="disclosueres item">
+          <h1>Disclosures</h1>
         </div>
-        <!-- <router-link></router-link> -->
-        <!-- <p>components place</p> -->
+        <div class="crypto_assets item">CryptoAssets</div>
       </div>
+
+      <div class="bottom_items">
+        <div class="market_watch item">MarketWatch</div>
+        <div class="pr_research item">PR/Research</div>
+      </div>
+      <!-- <div class="mainComponent_box">
+      </div> -->
     </div>
-    <!-- <router-link>+_+</router-link> -->
   </div>
 </template>
 
 <script>
-console.log('+_+');
-// import Header from '@/components/Header.vue';
-import Disclosures from '@/components/MainContents/Disclosures.vue';
-import CryptoAssets from '@/components/MainContents/CryptoAssets.vue';
-import MarketWatch from '@/components/MainContents/MarketWatch.vue';
-import PRResearch from '@/components/MainContents/PRResearch.vue';
 export default {
-  components: {
-    Disclosures,
-    CryptoAssets,
-    MarketWatch,
-    PRResearch,
-  },
+  components: {},
 };
 </script>
 
 <style lang="scss" scoped>
 @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;0,700;1,400&display=swap');
 @import url('https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css');
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
 
-  .landingPage_container {
-    /* width: 1200px; */
-    /* width: 100%; */
-    /* height: 100vh; */
-    /* padding-top: 10px; */
-    /* height: 360px; */
-    /* display: flex;
-    justify-content: center; */
-    /* align-items: center; */
-    /* margin-bottom: 100px; */
-    /* z-index: -1; */
-    flex-direction: column;
+.landingPage_container {
+  margin-top: 64px;
+  width: 100%;
+  /* display: flex;
+  justify-content: center;
+
+  flex-direction: column; */
+
+  .banner_container {
+    width: 100%;
+    height: 360px;
+    display: flex;
+    justify-content: center;
+    background-color: #090a0e;
     position: relative;
-    .banner_container {
-      width: 100%;
+
+    .banner_inner_container {
+      width: 1200px;
       height: 360px;
-      display: flex;
-      justify-content: center;
-      background-color: #090a0e;
-      /* border: 1px solid yellow; */
-      position: relative;
+      background-image: url('../assets/images/visual@2x.png');
+      background-repeat: no-repeat;
+      background-size: 604px 318px;
+      background-position: bottom right;
+      background-position: 100% 100%;
 
-      /* padding-top: 84px; */
-      .banner_inner_container {
-        /* width: 100%; */
-        width: 1200px;
-        height: 360px;
-        /* margin: 0 50px; */
-        /* margin-top: 50px; */
-        /* border: 1px solid whitesmoke; */
-        /* padding-left: 50px; */
-        /* padding: 84px 12px 0 0; */
-
-        /* padding-right: 12px; */
-        background-image: url('../assets/images/visual@2x.png');
-        background-repeat: no-repeat;
-        background-size: 604px 318px;
-        background-position: bottom right;
-        background-position: 100% 100%;
-
-        /* background-color: gray; */
-        .banner_contents {
-          /* width: 457px; */
-          /* position: absolute;
-          top: 100px; */
-          margin-top: 104px;
-          /* background-color: wheat; */
-          .banner_left {
-            p {
-              color: white;
-              font-size: 36px;
-              font-weight: bold;
-              font-family: 'poppins', 'Roboto', sans-serif;
-              margin-bottom: 24px;
-            }
-            .langding_banner_button {
-              width: 141px;
-              height: 48px;
-              background-color: #ffffff;
-              border-radius: 2px;
-              padding: 12px 24px;
-              border-style: none;
-              font-size: 14px;
-              font-weight: bold;
-              font-family: 'Spoqa Han Sans', sans-serif;
-            }
+      .banner_contents {
+        margin-top: 104px;
+        .banner_left {
+          p {
+            color: white;
+            font-size: 36px;
+            font-weight: bold;
+            font-family: 'poppins', 'Roboto', sans-serif;
+            margin-bottom: 24px;
+          }
+          .langding_banner_button {
+            width: 141px;
+            height: 48px;
+            background-color: #ffffff;
+            border-radius: 2px;
+            padding: 12px 24px;
+            border-style: none;
+            font-size: 14px;
+            font-weight: bold;
+            font-family: 'Spoqa Han Sans', sans-serif;
           }
         }
       }
     }
-    .ticker_container {
-      height: 42px;
-      background-color: #ffffff;
-      box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
-      p {
-        padding: 10px;
-        text-align: center;
+  }
+  .ticker_container {
+    height: 42px;
+    background-color: #ffffff;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    p {
+      padding: 10px;
+      text-align: center;
+    }
+  }
+  .mainContents_container {
+    max-width: 1200px;
+    /* width: 100%; */
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    padding: 20px;
+    background-color: rosybrown;
+
+    .top_items {
+      /* min-width: 1200px; */
+      /* max-width: 1200px; */
+      display: flex;
+      justify-content: center;
+      /* display: grid; */
+      /* grid-template-columns: 700fr 488fr; */
+      /* grid-template-columns: 700px 488px; */
+
+      gap: 20px;
+      /* display: flex; */
+      margin-bottom: 20px;
+
+      .disclosueres {
+        /* max-width: 1200px; */
+        /* flex-basis: 700px; */
+        /* width: 700px; */
+        /* max-width: 700px; */
+        height: 500px;
+        background-color: green;
+      }
+
+      .crypto_assets {
+        /* flex-basis: 488px; */
+        /* width: 488px; */
+        /* max-width: 488px; */
+        height: 500px;
+        background-color: darkcyan;
       }
     }
-    .mainContents_container {
-      max-width: 1280;
-      background-color: plum;
+
+    .bottom_items {
+      /* min-width: 1200px; */
+      display: flex;
+      /* display: grid;
+      grid-template-columns: 700px 488px; */
+      gap: 20px;
+
+      .market_watch {
+        /* width: 700px; */
+        height: 500px;
+        background-color: yellow;
+      }
+
+      .pr_research {
+        /* width: 488px; */
+        height: 500px;
+        background-color: rebeccapurple;
+      }
+    }
+  }
+}
+
+// 961px보다 크고 1200px보다 작을때
+@media all and (min-width: 961px) {
+  .mainContents_container {
+    max-width: 1200px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .top_items,
+    .bottom_items {
       display: flex;
       justify-content: center;
       align-items: center;
-      flex-direction: column;
+      width: 100%;
 
-      .mainContents_inner_container {
-        width: 1200px;
-        text-align: center;
-        background-color: rgb(168, 240, 240);
-        padding: 30px 0;
+      .disclosueres,
+      .market_watch {
+        width: 60%;
+        color: red;
+      }
+      .crypto_assets,
+      .pr_research {
+        color: white;
+        width: 40%;
+      }
+    }
+  }
+}
+// 601px보다 크고 960px보다 작을때
+// 600px ~ 960px 일 때
+@media all and (min-width: 601px) and (max-width: 960px) {
+  .mainContents_container {
+    max-width: 960px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    /* padding: 20px; */
+    background-color: yellow;
 
-        .mainComponent_box {
-          display: flex;
-          flex-wrap: wrap;
-          gap: 1em;
-          justify-content: space-around;
-        }
+    .top_items,
+    .bottom_items {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 100%;
+
+      .disclosueres,
+      .crypto_assets,
+      .market_watch,
+      .pr_research {
+        color: white;
+        width: 100%;
+      }
+    }
+  }
+}
+// 600px 보다 작을 때
+@media all and (max-width: 600px) {
+  .mainContents_container {
+    max-width: 600px;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    /* padding: 20px; */
+    background-color: yellow;
+
+    .top_items,
+    .bottom_items {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      width: 100%;
+
+      .disclosueres,
+      .crypto_assets,
+      .market_watch,
+      .pr_research {
+        color: white;
+        width: 100%;
+        height: 350px;
       }
     }
   }
--- src/views/test.vue
+++ src/views/test.vue
@@ -1,91 +1,228 @@
 <template>
-  <div class="layout_continer">
-    <div class="layout">
-      <!-- <aside>
-      사이드바
-    </aside> -->
-      <article>
-        <h1>타이틀</h1>
-        <div class="flexbox">
-          <div class="item item1">content1</div>
-          <div class="item item2">content2</div>
-          <div class="item item3">content3</div>
-          <div class="item item4">content4</div>
+  <div class="landingPage_container">
+    <div class="banner_container">
+      <div class="banner_inner_container">
+        <div class="banner_contents">
+          <div class="banner_left">
+            <p>Get only the notifications<br />that you really want.</p>
+            <button class="langding_banner_button">
+              <span>View Our App</span>
+            </button>
+          </div>
         </div>
-      </article>
+      </div>
+    </div>
+    <div class="ticker_container">
+      <p>ticker</p>
+    </div>
+    <div class="mainContents_container">
+      <div class="top_items">
+        <div class="disclosueres">
+          <h1>Disclosures</h1>
+
+          <div class="list_component">
+            <div class="list_component_name">1</div>
+            <div class="list_component_title">2</div>
+            <div class="list_component_price">3</div>
+            <div class="list_component_date">4</div>
+          </div>
+        </div>
+        <div class="crypto_assets">CryptoAssets</div>
+      </div>
+
+      <div class="bottom_items">
+        <div class="market_watch">MarketWatch</div>
+        <div class="pr_research">PR/Research</div>
+      </div>
+      <!-- <div class="mainComponent_box">
+      </div> -->
     </div>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  components: {},
+};
 </script>
 
 <style lang="scss" scoped>
-.layout_continer {
-  /* width: 100%; */
-  margin: 0 auto;
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;0,700;1,400&display=swap');
+@import url('https://spoqa.github.io/spoqa-han-sans/css/SpoqaHanSans-kr.css');
+
+.landingPage_container {
   display: flex;
   justify-content: center;
-  align-items: center;
-  /* flex-direction: column; */
-  .layout {
-    /* width: 1280px; */
-    width: 1200px;
-    margin-top: 100px;
-    /* padding: 20px; */
-    flex-wrap: nowrap;
-    gap: 1em;
+  flex-direction: column;
 
-    article {
-      /*  플렉스 아이템의 초기 크기를 지정 */
-      flex-basis: 1200px;
-      // 늘릴 비율
-      flex-grow: 1;
-      // 줄일 비율
-      flex-shrink: 1;
-      background-color: #ffe0e0;
+  .banner_container {
+    width: 100%;
+    height: 360px;
+    display: flex;
+    justify-content: center;
+    background-color: #090a0e;
+    position: relative;
 
-      /* display: flex;
-  justify-content: center; */
-      .flexbox {
-        display: flex;
-        flex-wrap: wrap;
-        gap: 1em;
-        /* padding: 10px; */
-        /* flex-basis: 1180px; */
-        background-color: #f0f0f0;
-      }
-      .item {
-        min-height: 500px;
+    .banner_inner_container {
+      width: 1200px;
+      height: 360px;
+      background-image: url('../assets/images/visual@2x.png');
+      background-repeat: no-repeat;
+      background-size: 604px 318px;
+      background-position: bottom right;
+      background-position: 100% 100%;
 
-        flex-basis: 480px;
-        flex-shrink: 1;
-        flex-grow: 1;
-
-        border: 1px solid green;
-      }
-      .item1,
-      .item3 {
-        /* flex: 2; */
-        width: 700px;
-        /* min-height: 500px; */
-        flex-shrink: 1;
-        flex-grow: 1;
-        /* flex-basis: 700px; */
-        background-color: darkcyan;
-      }
-      .item2,
-      .item4 {
-        width: 488px;
-        /* min-height: 500px; */
-        flex-shrink: 1;
-        flex-grow: 1;
-        /* flex-basis: 488px; */
-        /* flex: 1; */
-        background-color: silver;
+      .banner_contents {
+        margin-top: 104px;
+        .banner_left {
+          p {
+            color: white;
+            font-size: 36px;
+            font-weight: bold;
+            font-family: 'poppins', 'Roboto', sans-serif;
+            margin-bottom: 24px;
+          }
+          .langding_banner_button {
+            width: 141px;
+            height: 48px;
+            background-color: #ffffff;
+            border-radius: 2px;
+            padding: 12px 24px;
+            border-style: none;
+            font-size: 14px;
+            font-weight: bold;
+            font-family: 'Spoqa Han Sans', sans-serif;
+          }
+        }
       }
     }
+  }
+  .ticker_container {
+    height: 42px;
+    background-color: #ffffff;
+    box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.1);
+    p {
+      padding: 10px;
+      text-align: center;
+    }
+  }
+  .mainContents_container {
+    display: flex;
+    /* justify-content: space-between; */
+    align-items: center;
+    flex-direction: column;
+    /* max-width: 1200px; */
+    margin: 20px;
+    background-color: rosybrown;
+
+    /* .mainComponent_box {
+      min-width: 1200px;
+      background-color: rosybrown; */
+    /* display: grid; */
+    /* grid-template-columns: 2fr 1fr 2fr 1fr; */
+    /* gap: 10px; */
+    .top_items {
+      max-width: 1200px;
+      /* display: grid;
+      grid-template-columns: 700px 488px;
+      gap: 20px; */
+      display: flex;
+      margin-bottom: 20px;
+
+      .disclosueres {
+        /* max-width: 1200px; */
+        max-width: 700px;
+
+        height: 500px;
+        background-color: green;
+
+        .list_component {
+          display: flex;
+          background-color: white;
+
+          .list_component_name {
+            flex: 1;
+            border: 1px dotted gray;
+          }
+          .list_component_title {
+            flex: 2;
+            border: 1px dotted gray;
+          }
+          .list_component_price {
+            flex: 3;
+            border: 1px dotted gray;
+          }
+          .list_component_date {
+            flex: 4;
+            border: 1px dotted gray;
+          }
+        }
+      }
+
+      .crypto_assets {
+        /* width: 488px; */
+        height: 500px;
+        background-color: darkcyan;
+      }
+    }
+
+    .bottom_items {
+      display: grid;
+      grid-template-columns: 700px 488px;
+      gap: 20px;
+
+      .market_watch {
+        /* width: 700px; */
+        height: 500px;
+        background-color: yellow;
+      }
+
+      .pr_research {
+        /* width: 488px; */
+        height: 500px;
+        background-color: rebeccapurple;
+      }
+    }
+  }
+}
+
+// 961px보다 크고 1200px보다 작을때
+@media all and (min-width: 960px) {
+  /* .disclosueres,
+  .crypto_assets,
+  .market_watch,
+  .pr_research {
+    display: none;
+  } */
+
+  /* .top_items,
+  .bottom_items {
+    display: none;
+  } */
+}
+// 601px보다 크고 960px보다 작을때
+// 600px ~ 960px 일 때
+@media all and (min-width: 601px) and (max-width: 960px) {
+  // pc
+  .disclosueres,
+  .crypto_assets,
+  .market_watch,
+  .pr_research {
+    /* display: none; */
+  }
+}
+// 600px 보다 작을 때
+@media all and (max-width: 600px) {
+  .top_items,
+  .bottom_items {
+    display: block;
+  }
+
+  .disclosueres,
+  .crypto_assets,
+  .market_watch,
+  .pr_research {
+    width: 100%;
   }
 }
 </style>
